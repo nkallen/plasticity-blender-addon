b95335644c822936a3397db55accfbe56e041a2d
Nick Kallen <nickkallen@me.com>
2024-08-30 13:24:41 +0200

    v2.0.2

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ bl_info = {
     "name": "Plasticity",
     "description": "A bridge to Plasticity",
     "author": "Nick Kallen, User129863",
-    "version": (2, 0, 1),
+    "version": (2, 0, 2),
     "blender": (4, 1, 0),
     "location": "View3D > Sidebar > Plasticity",
     "category": "Object",
